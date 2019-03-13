2524860dc518d103b8e347ffa7d8b8b336c321a1
Denis Sholomitsky <subaru1634@mail.ru>
2019-03-13 09:40:14 +0300

    feat: add new function

--- src/index.js
+++ src/index.js
@@ -3,5 +3,14 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  preferences.unshift(0);
+  var count = 0;
+  for (var i = 1; i < preferences.length; i++) {
+    var a = i,
+        b = preferences[a],
+        c = preferences[b];
+    if (preferences[a] == a || preferences[b] == b || preferences[c] == c) continue;
+    if (preferences[c] == a) count += 1;
+  }
+return count/3;
 };
